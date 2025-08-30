990d8a0d9b9e9e0ac31a7c0f6d285f96493f9e3d
Razack <astrochimwala4@gmail.com>
2025-08-30 08:57:02 +0200

    Delete tts-app directory

--- tts-app/src/app/pages/auth/forgotpassword/page.tsx
+++
@@ -1,8 +0,0 @@
-import ForgotPassword from "@/app/components/FogotPassword";
-export default function ForgotPasswordPage() {
-  return (
-    <div>
-      <ForgotPassword />
-    </div>
-  );
-}
--- tts-app/src/app/pages/auth/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUp from "@/app/components/Signup";
-
-export default function SignUpPage() {
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
-}
